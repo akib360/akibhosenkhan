de00c540d2d8da2970914d9650568a9cbf4841b8
akib hosen khan <36074470+akib360@users.noreply.github.com>
2021-07-03 22:55:58 +0600

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -69,13 +69,9 @@ $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 if(mail($address, $e_subject, $msg, $headers)) {
 
 	// Email has sent successfully, echo a success page.
-
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h3>Email Sent Successfully.</h3>";
 	echo "<p class='text-muted'>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
-	echo "</div>";
-	echo "</fieldset>";
+	
+	
 
 } else {
 
